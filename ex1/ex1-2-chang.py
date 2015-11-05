4a5e0032e4c298178bbaf10249a6f206d32710ad
cliu <fluency.03@gmail.com>
2015-11-05 21:26:31 +0100

    ex1-2-chang.py

---
+++ ex1/ex1-2-chang.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+'''For scientific computing'''
+from numpy import *
+import scipy
+
+'''For plotting'''
+from matplotlib import pyplot, cm
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# Path of the files
+PATH = '/home/cliu/workspace/Andrew Ng/ex1/'
+
+
+
+
+
+
+# main function
+def main():
+
+
+
+
+# call the main function
+if __name__ == '__main__':
+	main()
