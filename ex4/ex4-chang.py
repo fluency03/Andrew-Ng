29f00bca8f009d86ca07bb2936d1395f6e5c438a
cliu <fluency.03@gmail.com>
2015-11-10 02:35:21 +0100

    ex4-chang.py

---
+++ ex4/ex4-chang.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+#!/usr/bin/python
+
+# import PIL.Image
+import scipy.optimize, scipy.special, scipy.io
+# import scipy.misc, 
+from numpy import *
+
+import pylab
+from matplotlib import pyplot, cm
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.mlab as mlab
+
+
+# Path of the files
+PATH = '/home/cliu/Documents/github/Andrew-Ng/ex4/'
+
+
+
+
+
+def part_1_1():
+	pass
+
+
+
+
+
+
+
+# main function
+def main():
+	part_1_1()
+
+
+# call the main function
+if __name__ == '__main__':
+	main()
+
+
