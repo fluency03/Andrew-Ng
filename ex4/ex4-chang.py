e650bdd6d1c9ee24b2415cefc8547c2308b67409
cliu <fluency.03@gmail.com>
2015-11-20 16:20:37 +0100

    ex4

--- ex4/ex4-chang.py
+++ ex4/ex4-chang.py
@@ -24,9 +24,9 @@ def sigmoid(z):
 	# return 1.0 / (1.0 + exp( -z ))
 
 
-def displayData( X, theta=None ):
+def displayData( X, theta1 = None, theta2 = None ):
 	width = 20 # width of one digital 
-	rows, cols = 10, 10 # row and column number of the display image in terms of digitals
+	rows, cols = 5, 5 # row and column number of the display image in terms of digitals
 	out = zeros( (width*rows, width*cols) ) # the initialization of digitals being displayed
 
 	rand_index = random.permutation( 5000 )[0: rows*cols]
@@ -39,6 +39,16 @@ def displayData( X, theta=None ):
 			y_index = y*width
 			out[x_index:x_index+width, y_index:y_index+width] = X[ rand_index[counter] ].reshape(width, width).T
 			counter += 1
+
+	if theta1 is not None and theta2 is not None:
+		result_matrix 	= []
+		
+		for idx in rand_index:
+			result = predict( X[idx], theta1, theta2 )
+			result_matrix.append( result )
+
+		result_matrix = array( result_matrix ).reshape( rows, cols ).transpose()
+		print result_matrix
 
 	img = scipy.misc.toimage(out)
 	pyplot.imshow(img)
@@ -97,9 +107,39 @@ def computeCost( nn_params, input_layer_size, hidden_layer_size, num_labels, X, 
 	return J
 
 
+def computeGradient( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda ):
+	Theta1, Theta2 = paramRollback( nn_params, input_layer_size, hidden_layer_size, num_labels )
+	# print shape(Theta1), shape(Theta2)
+	m, n   = shape(X)
+	a1, z2, a2, z3, h = feedForward( Theta1, Theta2, X, m )
+
+	yVec = ( (matlib.repmat(arange(1, num_labels+1), m, 1) == matlib.repmat(y, 1, num_labels)) + 0)
+
+	D1 = zeros(shape(Theta1))
+	D2 = zeros(shape(Theta2))
+
+	sigma3 = h - yVec.T
+	sigma2 = Theta2.T.dot(sigma3) * sigmoidGradient( r_[ ones((1,m)), z2 ] )
+	sigma2 = sigma2[1:,:]
+	# print shape(sigma2)
+	delta1 = sigma2.dot( a1.T )
+	delta2 = sigma3.dot( a2.T ) 
+
+	D1[:,1:] = delta1[:,1:]/m + (Theta1[:,1:] * lamda / m)
+	D2[:,1:] = delta2[:,1:]/m + (Theta2[:,1:] * lamda / m)
+	D1[:,0]  = delta1[:,0]/m
+	D2[:,0]  = delta2[:,0]/m 
+
+	grad = array([D1.T.reshape(-1).tolist() + D2.T.reshape(-1).tolist()]).T
+	# print D1, shape(D1)
+	# print D2, shape(D2)
+
+
+	return ndarray.flatten(grad)
+
+
 def nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda ):
-	Theta1 = nn_params[0]
-	Theta2 = nn_params[1]
+	Theta1, Theta2 = paramRollback( nn_params, input_layer_size, hidden_layer_size, num_labels )
 	# print shape(Theta1), shape(Theta2)
 	m, n   = shape(X)
 	a1, z2, a2, z3, h = feedForward( Theta1, Theta2, X, m )
@@ -131,13 +171,14 @@ def nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, 
 
 	D1[:,1:] = delta1[:,1:]/m + (Theta1[:,1:] * lamda / m)
 	D2[:,1:] = delta2[:,1:]/m + (Theta2[:,1:] * lamda / m)
-	D1[:,0] = delta1[:,0]/m
-	D2[:,0] = delta2[:,0]/m 
+	D1[:,0]  = delta1[:,0]/m
+	D2[:,0]  = delta2[:,0]/m 
 
+	grad = array([D1.T.reshape(-1).tolist() + D2.T.reshape(-1).tolist()]).T
 	# print D1, shape(D1)
 	# print D2, shape(D2)
 
-	return J, D1, D2
+	return J, grad
 
 
 def randInitializeWeights( L_in, L_out ):
@@ -180,15 +221,40 @@ def checkNNGradients( lamda=0 ):
 
 	Theta1 = debugInitializeWeights(hidden_layer_size, input_layer_size)
 	Theta2 = debugInitializeWeights(num_labels, hidden_layer_size)
+	# print shape(Theta1), shape(Theta2)
 
 	X  = debugInitializeWeights(m, input_layer_size - 1)
 	y  = 1 + mod(m, num_labels).T
 
-	nn_params 	= array([theta1.T.reshape(-1).tolist() + theta2.T.reshape(-1).tolist()]).T
+	nn_params 	= r_[Theta1.T.flatten(), Theta2.T.flatten()]
+	# print shape(nn_params)
 	gradient 	= nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda )[1]
 	numgrad 	= computeNumericalGradient( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda )
-	diff = linalg.norm( numgrad - gradient ) / (linalg.norm( numgrad + gradient ))
+	diff 		= linalg.norm( numgrad - gradient ) / (linalg.norm( numgrad + gradient ))
 	print diff
+
+
+def recodeLabel( y, k ):
+	m = shape(y)[0]
+	out = zeros( ( k, m ) )
+	
+	for i in range(0, m):
+		out[y[i]-1, i] = 1
+
+	return out
+
+
+def predict( X, theta1, theta2 ):
+	a1 = r_[ones((1, 1)), X.reshape( shape(X)[0], 1 )]
+	z2 = sigmoid( theta1.dot( a1 ))
+	z2 = r_[ones((1, 1)), z2]
+	z3 = sigmoid(theta2.dot( z2 ))
+	return argmax(z3) + 1
+
+
+
+
+
 
 
 def part_1():
@@ -206,12 +272,14 @@ def part_1():
 
 	weight = scipy.io.loadmat( PATH + "ex4weights.mat" )
 	Theta1, Theta2 = weight['Theta1'], weight['Theta2']
-	nn_params = [ Theta1, Theta2 ]
+	# nn_params = [ Theta1, Theta2 ]
+	nn_params = r_[Theta1.T.flatten(), Theta2.T.flatten()]
 
 
-	nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda[0])
+	J0 = nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda[0])[0]
 	# Regularized cost function
-	nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda[1])
+	J1 = nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda[1])[0]
+	print J0, J1
 
 
 def part_2():
@@ -230,7 +298,9 @@ def part_2():
 
 	weight = scipy.io.loadmat( PATH + "ex4weights.mat" )
 	Theta1, Theta2 = weight['Theta1'], weight['Theta2']
-	nn_params = [ Theta1, Theta2 ]
+	# nn_params = [ Theta1, Theta2 ]
+	nn_params = r_[Theta1.T.flatten(), Theta2.T.flatten()]
+
 
 	input_layer_size  	= 400
 	hidden_layer_size 	= 25
@@ -238,11 +308,8 @@ def part_2():
 	lamda 				= 1
 
 
-	J, D1, D2 = nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda)
-
-	# unroll theta
-	theta = array([D1.reshape(-1).tolist() + D2.reshape(-1).tolist()]).T
-	# print shape(theta)
+	J, grad = nnCostFunction( nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda)
+	# print shape(grad)
 
 	#--------------------------this part is incredibly slow--------------------------------------------#
 	# grad = computeNumericalGradient( theta, input_layer_size, hidden_layer_size, num_labels, X, y, lamda )
@@ -250,7 +317,60 @@ def part_2():
 	#--------------------------------------------------------------------------------------------------#
 
 	# part_2_4
+	checkNNGradients()
+
+	# part_2_5
+	checkNNGradients( 1 )
+
+
+
+def part_2_6():
+	# part_2_3
+	data = scipy.io.loadmat( PATH + "ex4data1.mat" )
+	X, y = data['X'], data['y']
+	m, n = shape(X)
+
+	# weight = scipy.io.loadmat( PATH + "ex4weights.mat" )
+	# Theta1, Theta2 = weight['Theta1'], weight['Theta2']
+
+	theta1 = randInitializeWeights( 400, 25 )
+	theta2 = randInitializeWeights( 25, 10 )
+	params = r_[theta1.T.flatten(), theta2.T.flatten()]
+
+	input_layer_size  	= 400
+	hidden_layer_size 	= 25
+	num_labels 			= 10
+	lamda 				= 1
+
+	yk 	   = recodeLabel( y, num_labels )
+	X_bias = r_[ ones((1, shape(X)[0] )), X.T]
+
+	result = scipy.optimize.fmin_cg( computeCost, x0=params, \
+									args=(input_layer_size, hidden_layer_size, num_labels, X, y, lamda), \
+									fprime=computeGradient, \
+									maxiter=400, disp=True, full_output=True )
+	# result = scipy.optimize.fmin_cg( computeCost, x0=params, args=(input_layer_size, hidden_layer_size, num_labels, X, y, lamda), fprime=computeGradient, maxiter=50, disp=True, full_output=True )
+	print result[1]
+	theta1, theta2 	= paramRollback( result[0], input_layer_size, hidden_layer_size, num_labels )
+
+	displayData( X, theta1, theta2 )
+	# displayData( theta1[:,1:] )
+
+	counter = 0
+	for i in range(0, m):
+		prediction = predict( X[i], theta1, theta2 )
+		actual = y[i]
+		if( prediction == actual ):
+			counter+=1
+	print "Prediction: %.6f" %((counter * 100 / m)/100)
+
+	displayData( theta1[:,1:] )
+
 	
+
+def part_3():
+	pass
+
 
 
 
@@ -261,7 +381,8 @@ def part_2():
 def main():
 	part_1()
 	part_2()
-
+	part_2_6()
+	part_3()
 
 # call the main function
 if __name__ == '__main__':
