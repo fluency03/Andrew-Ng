d3b6581e1bd62d388afdc71fd981980e8f031254
cliu <fluency.03@gmail.com>
2015-11-20 16:28:03 +0100

    ex5

---
+++ ex5/ex5-chang.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# import PIL.Image
+import scipy.optimize, scipy.special, scipy.io
+# import scipy.misc, 
+from numpy import * 
+from numpy import matlib
+# import numpy.matlib
+
+import pylab
+from matplotlib import pyplot, cm
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.mlab as mlab
+
+
+# Path of the files
+PATH = '/home/cliu/Documents/github/Andrew-Ng/ex5/'
+
+
+def part_1():
+	pass
+
+
+
+# main function
+def main():
+	part_1()
+
+# call the main function
+if __name__ == '__main__':
+	main()
+
+
