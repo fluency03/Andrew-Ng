9a4e318efc81fb46d6be4ddd7dfaac2f2b5c2293
cliu <fluency.03@gmail.com>
2015-11-22 17:04:20 +0100

    ex6

---
+++ ex6/ex6-chang.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import scipy.optimize, scipy.special, scipy.io
+from numpy import * 
+from numpy import matlib
+
+import pylab
+from matplotlib import pyplot, cm
+from util import Util
+
+# Path of the files
+PATH = '/home/cliu/Documents/github/Andrew-Ng/ex6/'
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------
+
+def part_1():
+	pass
+
+
+
+# ---------------------------------------------------------------------------------
+
+# main function
+def main():
+	part_1()
+
+
+
+
+
+# call the main function
+if __name__ == '__main__':
+	main()
+
+
